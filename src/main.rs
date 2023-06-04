80261222c67c1e7e9b60696eb29a94f205749b3b
guemax <github-mg@guenther.cloud>
2023-06-04 12:48:38 +0200

    Add startup and exit messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Dies ist der Einmaleins-Trainer v{}.", env!("CARGO_PKG_VERSION"));
+    println!("Das Training wird nun gestartet. (Drücken Sie Ctrl+C zum Beenden.)\n");
+
+    println!("Wollen sie das Training wirklich beenden? [j/N]");
+    println!("\nSie haben 9 von 10 Fragen richtig beanwortet. Dies entspricht einer Erfolgsrate von 90%.");
+    println!("Herzlichen Glückwunsch!");
 }
