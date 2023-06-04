a24d688a706d8f09a381146ccfe10f74806cd7cf
guefra <123297340+guefra@users.noreply.github.com>
2023-06-04 16:07:32 +0200

    Show the numbers on the screen
    Show the question 'Was ist x*y?' on the top-cen
    ter of the window, after the random numbers have been generated

--- src/einmaleins_trainer.py
+++ src/einmaleins_trainer.py
@@ -1,16 +1,24 @@
 import random
 
-from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        numbers()
+        qLabelText = QLabel(f'Was ist {number1} * {number2}?', alignment=Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        font = qLabelText.font()
+        font.setPointSize(20)
+        qLabelText.setFont(font)
+
         # Set the window's name and size
         self.setWindowTitle("Einmaleins Trainer")
         self.setFixedSize(QSize(500, 400))
+
+        self.setCentralWidget(qLabelText)
 
 
 def numbers():
