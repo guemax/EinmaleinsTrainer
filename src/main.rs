db428d2fb4ebb19bde4fa0c88aa62d8173358111
guemax <github-mg@guenther.cloud>
2023-06-04 13:01:19 +0200

    Add basic output for question

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ fn main() {
     println!("Dies ist der Einmaleins-Trainer v{}.", env!("CARGO_PKG_VERSION"));
     println!("Das Training wird nun gestartet. (Drücken Sie Ctrl+C zum Beenden.)\n");
 
-    println!("Wollen sie das Training wirklich beenden? [j/N]");
+    let indentation = "    ";
+
+    loop {
+	let num_1 = 7;
+	let num_2 = 5;
+	println!("{indentation}{num_1} ✕ {num_2} = ");
+	break;
+    }
+
+    println!("\nWollen sie das Training wirklich beenden? [j/N]");
     println!("\nSie haben 9 von 10 Fragen richtig beanwortet. Dies entspricht einer Erfolgsrate von 90%.");
     println!("Herzlichen Glückwunsch!");
 }
