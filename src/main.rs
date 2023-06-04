6299021ff57e954f3a00be1439e3a51fa86c60f5
guemax <github-mg@guenther.cloud>
2023-06-04 19:35:40 +0200

    Refactor game loop

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::io::Write;
+use std::num::ParseIntError;
 
 extern crate termion;
 use termion::color;
@@ -31,60 +32,83 @@ fn align_equation_at_equal_and_multiplication_sign(num_1: u32, num_2: u32, resul
     let result_string: String;
 
     num_1_string = match num_1.to_string().len() {
-	1 => format!(" {num_1}"),
-	2 => format!("{num_1}"),
-	_ => todo!()
+		1 => format!(" {num_1}"),
+		2 => format!("{num_1}"),
+		_ => todo!()
     };
     num_2_string = match num_2.to_string().len() {
-	1 => format!("{num_2} "),
-	2 => format!("{num_2}"),
-	_ => todo!()
+		1 => format!("{num_2} "),
+		2 => format!("{num_2}"),
+		_ => todo!()
     };
     result_string = match result.to_string().len() {
-	1 => format!("  "),
-	2 => format!(" "),
-	3 => format!(""),
-	_ => todo!()
+		1 => format!("  "),
+		2 => format!(" "),
+		3 => format!(""),
+		_ => todo!()
     };
     return format!("{num_1_string} ✕ {num_2_string} = {result_string}");
 }
 
 
-fn main() {
-    println!("Dies ist der Einmaleins-Trainer v{}.", env!("CARGO_PKG_VERSION"));
-    println!("Das Training wird nun gestartet. (Drücken Sie Ctrl+C zum Beenden.)\n");
+fn generate_numbers() -> (u32, u32) {
+    (rand::thread_rng().gen_range(1..=10), rand::thread_rng().gen_range(1..=10))
+}
 
-    let indentation = "    ";
+
+fn start_up_message() {
+	println!("Dies ist der Einmaleins-Trainer v{}.", env!("CARGO_PKG_VERSION"));
+    println!("Das Training wird nun gestartet. (Drücken Sie Ctrl+C zum Beenden.)\n");
+}
+
+
+fn exit_message() {
+    println!("\nSie haben 9 von 10 Fragen richtig beanwortet. Dies entspricht einer Erfolgsrate von 90%.");
+    println!("Herzlichen Glückwunsch!");
+}
+
+
+fn ask_question(first_factor: u32, second_factor: u32, product: u32) {
+	let indentation = "    ";
+
+	print!("{}{}", indentation, align_equation_at_equal_and_multiplication_sign(first_factor, second_factor, product));
+	io::stdout().flush().unwrap();
+}
+
+
+fn get_answer() -> Result<u32, ParseIntError> {
+	let mut answer = String::new();
+
+	io::stdin().read_line(&mut answer).expect("Die Anwort konnte nicht gelesen werden.");
+	let answer: u32 = answer.trim().parse()?;
+	return Ok(answer);
+}
+
+
+fn main() {
+    start_up_message();
 
     loop {
-	let num_1 = rand::thread_rng().gen_range(1..=10);
-	let num_2 = rand::thread_rng().gen_range(1..=10);
+		let (first_factor, second_factor) = generate_numbers();
+		let product = first_factor * second_factor;
 
-	print!("{}{}", indentation, align_equation_at_equal_and_multiplication_sign(num_1, num_2, num_1 * num_2));
-	std::io::stdout().flush().unwrap();
+		ask_question(first_factor, second_factor, product);
+		let answer = get_answer();
 
-	let mut answer = String::new();
-	io::stdin()
-	    .read_line(&mut answer)
-	    .expect("Antwort konnte nicht eingelesen werden.");
-	
-	let answer: u32 = match answer.trim().parse() {
-	    Ok(answer) => answer,
-	    Err(_) => {
-		println!("{}Bitte gib eine Zahl als Antwort ein.{}", color::Fg(color::Red), color::Fg(color::Reset));
-		continue;
-	    }
-	};
-	
-	if answer == (num_1 * num_2) {
-	    print_after_answer(fg_green("✔".to_string()));
-	} else {
-	    let text = format!("✘ (Richtige Antwort: {})", num_1 * num_2);
-	    print_after_answer(fg_red(text));
-	}
+		match answer {
+			Err(_) => print_after_answer(fg_red(format!("✘ Bitte gib eine Zahl als Antwort ein."))),
+			Ok(answer) => {
+				let expected_answer = product;
+				if answer == expected_answer {
+					print_after_answer(fg_green("✔".to_string()));
+				} else {
+					let text = format!("✘ (Richtige Antwort: {})", product);
+					print_after_answer(fg_red(text));
+				}
+			}
+		}
     }
 
     println!("\nWollen sie das Training wirklich beenden? [j/N]");
-    println!("\nSie haben 9 von 10 Fragen richtig beanwortet. Dies entspricht einer Erfolgsrate von 90%.");
-    println!("Herzlichen Glückwunsch!");
+	exit_message();
 }
